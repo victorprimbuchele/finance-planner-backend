1b4dac34f4e8bb488b61eae40c7c64db3ff728f0
Victor Prim <victorprimbuchele@gmail.com>
2022-04-21 15:18:09 -0300

    fix: adicionado try catch nos controllers

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,6 @@ import { auth } from "./middlewares/auth";
 const router = Router();
 
 router.post("/users", UserController.create);
-router.get("/login", UserController.login);
-router.get("/coconut123", auth, (req, res) => {
-  return res.json({
-    message: "Hello World",
-  });
-});
+router.post("/login", UserController.login);
 
 export default router;
