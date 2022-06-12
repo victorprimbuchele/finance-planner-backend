b07d196443a27fa75b7239de32dc1aec33202e90
Victor Prim <victorprimbuchele@gmail.com>
2022-06-12 00:59:20 -0300

    feature/Criado modelo, tabela no banco, controlador e rota para métodos de pagamento, vinculado ao usuário

---
+++ src/app/controllers/PaymentMethodController.ts
@@ -0,0 +1,172 @@
+import { Request, Response } from "express";
+import { prisma } from "../../database/migrations/connect";
+import { getToken } from "../../utils/bearerDecrypt";
+
+// controlador de métodos de pagamento com crud
+class PaymentMethodController {
+  // criar novo método de pagamento
+  async create(req: Request, res: Response) {
+    const { name } = req.body;
+
+    const decryptToken = getToken(req, res);
+
+    if (!decryptToken) {
+      return null;
+    }
+
+    try {
+      if (name == null) {
+        return res.status(400).json({
+          status: "error",
+          message: "Name cannot be null",
+        });
+      }
+
+      if (name.length < 3) {
+        return res.status(400).json({
+          status: "error",
+          message: "Name must be at least 3 characters",
+        });
+      }
+
+      const newPaymentMethod = await prisma.paymentMethod.create({
+        data: {
+          name,
+          userId: decryptToken.id,
+        },
+      });
+
+      return res.status(201).json({
+        status: "success",
+        data: newPaymentMethod,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "Internal server error. (Error code: 50)",
+      });
+    }
+  }
+  // listar todos os métodos de pagamento
+  async list(req: Request, res: Response) {
+    try {
+      const decryptToken = getToken(req, res);
+
+      if (!decryptToken) {
+        return null;
+      }
+
+      const paymentMethod = prisma.paymentMethod;
+
+      const allPaymentMethods = await paymentMethod.findMany({
+        where: {
+          userId: decryptToken.id,
+        },
+      });
+
+      return res.status(200).json({
+        status: "success",
+        data: allPaymentMethods,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "Internal server error. (Error code: 50)",
+      });
+    }
+  }
+  // atualizar método de pagamento
+  async update(req: Request, res: Response) {
+    const { name } = req.body;
+    const { id } = req.params;
+    const numberId = Number(id);
+
+    try {
+      if (name == null) {
+        return res.status(400).json({
+          status: "error",
+          message: "Name cannot be null",
+        });
+      }
+
+      if (name.length < 3) {
+        return res.status(400).json({
+          status: "error",
+          message: "Name must be at least 3 characters",
+        });
+      }
+
+      if (!id) {
+        return res.status(400).json({
+          status: "error",
+          message: "Id cannot be null",
+        });
+      }
+
+      const newPaymentMethod = await prisma.paymentMethod.update({
+        where: {
+          id: numberId,
+        },
+        data: {
+          name,
+        },
+      });
+
+      return res.status(200).json({
+        status: "success",
+        data: newPaymentMethod,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "Internal server error. (Error code: 50)",
+      });
+    }
+  }
+  // deletar método de pagamento
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    const numberId = Number(id);
+    const paymentMethod = prisma.paymentMethod;
+
+    try {
+      if (!id) {
+        return res.status(400).json({
+          status: "error",
+          message: "Id cannot be null",
+        });
+      }
+
+      const isPaymentMethod = await paymentMethod.findUnique({
+        where: {
+          id: numberId,
+        },
+      });
+
+      if (!isPaymentMethod) {
+        return res.status(400).json({
+          status: "error",
+          message: "Payment method not found",
+        });
+      }
+
+      const deletePaymentMethod = await paymentMethod.delete({
+        where: {
+          id: numberId,
+        },
+      });
+
+      return res.status(200).json({
+        status: "success",
+        data: deletePaymentMethod,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "Internal server error. (Error code: 50)",
+      });
+    }
+  }
+}
+
+export default new PaymentMethodController();
